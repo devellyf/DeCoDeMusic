06258ed1649a00b14d6f6c39e544cb046c7c5a91
KANNU xd <90442087+devellyf@users.noreply.github.com>
2021-12-05 03:11:02 +0530

    Update ytsearch.py

--- plugins/ytsearch.py
+++ plugins/ytsearch.py
@@ -1,6 +1,19 @@
-from pyrogram import Client as app, filters
+import logging
+
 from pyrogram.types import Message
+from search_engine_parser import GoogleSearch
 from youtube_search import YoutubeSearch
+
+from pyrogram import Client as app, filters
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+import pyrogram
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 @app.on_message(pyrogram.filters.command(["search"]))
@@ -11,9 +24,9 @@ async def ytsearch(_, message: Message):
             return
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("Searching....")
-        results = YoutubeSearch(query, max_results=5).to_dict()
+        results = YoutubeSearch(query, max_results=4).to_dict()
         text = ""
-        for i in range(5):
+        for i in range(4):
             text += f"Title - {results[i]['title']}\n"
             text += f"Duration - {results[i]['duration']}\n"
             text += f"Views - {results[i]['views']}\n"
@@ -22,3 +35,4 @@ async def ytsearch(_, message: Message):
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
         await message.reply_text(str(e))
+
